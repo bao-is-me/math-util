ce8bbee0a75b203bba487e7d8cceb19872bea5c2
bao-is-me <baoisme404@gmail.com>
2025-03-13 16:43:27 +0700

    update main code to use recursion

--- src/main/java/com/baoisme/mathutil/core/MathUtility.java
+++ src/main/java/com/baoisme/mathutil/core/MathUtility.java
@@ -10,24 +10,47 @@ public class MathUtility {
     // mà mình chỉ tính từ 0..... 20!
     // hàm nhận vào n, trả về n! ( n tỏng đoạn từ [0....20]
     // 1 lấy reqs, 2 test case
-    public static long getFactorial(int n)
-    {
-        //kĩ thuật nhồi con heo đất, ốc nhồi thịt, tiếp chiêu
-        // kĩ thuật nhân dồn, cộng ồn giá trí
-        if ( n < 0 ||  n > 20)
-            throw new IllegalArgumentException("Invalid n. n must be in range [0, 20]");
-        long result = 1;
-        if (n == 0)
-            return 1; //thoát sớm khi có thể, return ngay khi có thể
-        // if av for chỉ có 1 lệnh kh cần {.....1 lệnh}
-        //return sớm kh cần dùng luôn
 
-        for (int i = 1; i <= n; i++) {
-//          result = result * i;
-            result *= i; //nhan dồn từ 1 đến n, i tăng từ 1 2 3, dồn vào results
-        }
-        return result;
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20){
+            throw new IllegalArgumentException("Invalid n. n must be in range [0, 20]");
     }
+        if (n == 0 || n == 1)
+            return 1;
+        return  n * getFactorial(n - 1);
+    }
+
+
+    //5! = 1.2.3.4.5
+    //   =   4! x 5
+    //4! = 3! x 4
+    //3! = 2! x 3
+    //2! = 1! x 2
+    //1! = 1
+    //đệ quy - recursion
+    //gội lại chính mình với quy mô nhỏ hơn - búp bê nga
+    //n! = n x (n-1)!
+
+
+//    public static long getFactorial(int n)
+//    {
+//        //kĩ thuật nhồi con heo đất, ốc nhồi thịt, tiếp chiêu
+//        // kĩ thuật nhân dồn, cộng ồn giá trí
+//        if ( n < 0 ||  n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be in range [0, 20]");
+//        long result = 1;
+//        if (n == 0)
+//            return 1; //thoát sớm khi có thể, return ngay khi có thể
+//        // if av for chỉ có 1 lệnh kh cần {.....1 lệnh}
+//        //return sớm kh cần dùng luôn
+//
+//        for (int i = 1; i <= n; i++) {
+////          result = result * i;
+//            result *= i; //nhan dồn từ 1 đến n, i tăng từ 1 2 3, dồn vào results
+//        }
+//        return result;
+//    }
 
 
 }
